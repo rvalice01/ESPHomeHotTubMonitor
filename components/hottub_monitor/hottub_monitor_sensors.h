535ff998d41f1cd114ba1212d365148ebf79ea1c
rvalice01 <rvalice@gmail.com>
2026-03-31 16:16:38 -0400

    change update interval from 1s to on change

--- components/hottub_monitor/hottub_monitor_sensors.h
+++ components/hottub_monitor/hottub_monitor_sensors.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "hottub_monitor.h"
@@ -19,28 +18,67 @@ class HotTubSensorBase : public PollingComponent {
   sensor::Sensor *sensor_{nullptr};
 };
 
+// Error messages (int/bitmask)
 class ErrorMessagesSensor : public HotTubSensorBase {
  public:
   using HotTubSensorBase::HotTubSensorBase;
+
   void update() override {
-    if (sensor_ != nullptr) sensor_->publish_state(parent_->get_error_messages());
+    if (sensor_ == nullptr) return;
+
+    int v = parent_->get_error_messages();
+    if (!has_last_ || v != last_) {
+      last_ = v;
+      has_last_ = true;
+      sensor_->publish_state(v);
+    }
   }
+
+ protected:
+  bool has_last_{false};
+  int last_{0};
 };
 
+// Measured temperature (int)
 class MeasuredTemperatureSensor : public HotTubSensorBase {
  public:
   using HotTubSensorBase::HotTubSensorBase;
+
   void update() override {
-    if (sensor_ != nullptr) sensor_->publish_state(parent_->get_measured_temp());
+    if (sensor_ == nullptr) return;
+
+    int v = parent_->get_measured_temp();
+    if (!has_last_ || v != last_) {
+      last_ = v;
+      has_last_ = true;
+      sensor_->publish_state(v);
+    }
   }
+
+ protected:
+  bool has_last_{false};
+  int last_{0};
 };
 
+// Light status (int enum)
 class LightStatusSensor : public HotTubSensorBase {
  public:
   using HotTubSensorBase::HotTubSensorBase;
+
   void update() override {
-    if (sensor_ != nullptr) sensor_->publish_state(parent_->get_light_state());
+    if (sensor_ == nullptr) return;
+
+    int v = parent_->get_light_state();
+    if (!has_last_ || v != last_) {
+      last_ = v;
+      has_last_ = true;
+      sensor_->publish_state(v);
+    }
   }
+
+ protected:
+  bool has_last_{false};
+  int last_{0};
 };
 
 }  // namespace hottub_monitor
